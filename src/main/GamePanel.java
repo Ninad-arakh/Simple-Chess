1b4f0ce2d3cadeb37106ad53cc42ef792516ce55
Ninad-arakh <arakhninad@gmail.com>
2024-09-19 00:02:10 +0530

    moving pieces White

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -17,40 +18,43 @@ import piece.Queen;
 import piece.Rook;
 
 public class GamePanel extends JPanel implements Runnable {
-	
+
 	public static final int Width = 1100;
 	public static final int Height = 800;
 	final int FPS = 60;
 	Thread gameThread;
 	Board board = new Board();
-	
-	
-	//pieces
+	Mouse mouse = new Mouse();
+
+	// pieces
 	public static ArrayList<Piece> pieces = new ArrayList<Piece>();
 	public static ArrayList<Piece> simPieces = new ArrayList<Piece>();
-	
-	
-	//color
+	Piece activeP;
+
+	// color
 	public static final int WHITE = 0;
 	public static final int BLACK = 1;
 	int currentColor = WHITE;
-	
-	public GamePanel () {
+
+	public GamePanel() {
 		setPreferredSize(new Dimension(Width, Height));
 		setBackground(Color.black);
-		
+
+		addMouseMotionListener(mouse);
+		addMouseListener(mouse);
+
 		setPieces();
 		copyPieces(pieces, simPieces);
 	}
-	
+
 	public void launchGame() {
 		gameThread = new Thread(this);
 		gameThread.start();
 	}
-	
+
 	public void setPieces() {
-		
-		//whiteTeam
+
+		// whiteTeam
 		pieces.add(new Pawn(0, 6, WHITE));
 		pieces.add(new Pawn(1, 6, WHITE));
 		pieces.add(new Pawn(2, 6, WHITE));
@@ -67,7 +71,7 @@ public class GamePanel extends JPanel implements Runnable {
 		pieces.add(new Bishop(5, 7, WHITE));
 		pieces.add(new King(4, 7, WHITE));
 		pieces.add(new Queen(3, 7, WHITE));
-		
+
 		// BlackTeam
 		pieces.add(new Pawn(0, 1, BLACK));
 		pieces.add(new Pawn(1, 1, BLACK));
@@ -86,54 +90,99 @@ public class GamePanel extends JPanel implements Runnable {
 		pieces.add(new King(4, 0, BLACK));
 		pieces.add(new Queen(3, 0, BLACK));
 	}
-	
+
 	private void copyPieces(ArrayList<Piece> source, ArrayList<Piece> target) {
 		target.clear();
-		for(int i = 0; i< source.size(); i++) {
+		for (int i = 0; i < source.size(); i++) {
 			target.add(source.get(i));
 		}
 	}
-	
+
 	@Override
 	public void run() {
-		
+
 		// gameloop
-		double drawInterval = 1000000000/FPS;
+		double drawInterval = 1000000000 / FPS;
 		double delta = 0;
 		long lastTime = System.nanoTime();
 		long currentTime;
-		
-		while(gameThread != null) {
+
+		while (gameThread != null) {
 			currentTime = System.nanoTime();
-			
-			delta += (currentTime - lastTime)/drawInterval;
-			lastTime= currentTime;
-			
-			if(delta >= 1) {
+
+			delta += (currentTime - lastTime) / drawInterval;
+			lastTime = currentTime;
+
+			if (delta >= 1) {
 				update();
 				repaint();
 				delta--;
 			}
 		}
 	}
-	
+
 	private void update() {
-		
-	}
-	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		Graphics2D g2 = (Graphics2D)g;
-		
-		board.draw(g2);
-		
-		//pieces
-		for(Piece p : simPieces) {
-			p.draw(g2);
+
+		//////// Mouse Button Pressed////////////
+		if (mouse.pressed) {
+
+			if (activeP == null) {
+				// if the activeP is null check if you can pick up a piece
+				for (Piece piec : simPieces) {
+					// if the mouse is on Ally piece, pick it up as the activeP
+					if (piec.color == currentColor && piec.col == mouse.x / board.SQUARE_SIZE
+							&& piec.row == mouse.y / board.SQUARE_SIZE) {
+
+						activeP = piec;
+
+					}
+				}
+			} else {
+				// if the player is holding a piece simulate the move;
+				simulate();
+			}
 		}
+
+		// Mouse Button released //
+		if (mouse.pressed == false) {
+			if (activeP != null) {
+				activeP.updatePosition();
+				activeP = null;
+			}
+		}
+
 	}
 
-	
+	public void simulate() {
+
+		// if a piece is being held, update it's position
+		activeP.x = mouse.x - board.HALF_SQUARE_SIZE;
+		activeP.y = mouse.y - board.HALF_SQUARE_SIZE;
+		activeP.col = activeP.getCol(activeP.x);
+		activeP.row = activeP.getRow(activeP.y);
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		Graphics2D g2 = (Graphics2D) g;
+
+		board.draw(g2);
+
+		// pieces
+		for (Piece p : simPieces) {
+			p.draw(g2);
+		}
+
+		if (activeP != null) {
+			g2.setColor(Color.white);
+			g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7f));
+			g2.fillRect(activeP.col * Board.SQUARE_SIZE, activeP.row * Board.SQUARE_SIZE, Board.SQUARE_SIZE,
+					Board.SQUARE_SIZE);
+			g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+
+			activeP.draw(g2);
+		}
+	}
 
 }
